b0b9e709e599b60f318bbce55691c55b11867106
Alex Charland <ac3charland@gmail.com>
2020-05-11 11:33:24 -0500

    Ignore local credentials. Remove Ikea registry link.

--- src/components/registry/registry.jsx
+++ src/components/registry/registry.jsx
@@ -13,9 +13,9 @@ export default function Registry(props) {
             </Fade>
             <Fade bottom>
                 <div className={`${cb}__button-flex`}>
-                    <a className={`${cb}__button`} target='_blank' href={amazonRegistryUrl}><div className={`${cb}__button-content`}>Amazon<span className='sr-only'>External link: opens a new tab</span></div></a>
-                    <a className={`${cb}__button`} target='_blank' href={bbbRegistryUrl}><div className={`${cb}__button-content`}>Bed Bath & Beyond<span className='sr-only'>External link: opens a new tab</span></div></a>
-                    <a className={`${cb}__button`} target='_blank' href={ikeaRegistryUrl}><div className={`${cb}__button-content`}>Ikea<span className='sr-only'>External link: opens a new tab</span></div></a>
+                    <a className={`${cb}__button`} target='_blank' rel="noopener noreferrer" href={amazonRegistryUrl}><div className={`${cb}__button-content`}>Amazon<span className='sr-only'>External link: opens a new tab</span></div></a>
+                    <a className={`${cb}__button`} target='_blank' rel="noopener noreferrer" href={bbbRegistryUrl}><div className={`${cb}__button-content`}>Bed Bath & Beyond<span className='sr-only'>External link: opens a new tab</span></div></a>
+                    {/* <a className={`${cb}__button`} target='_blank' rel="noopener noreferrer" href={ikeaRegistryUrl}><div className={`${cb}__button-content`}>Ikea<span className='sr-only'>External link: opens a new tab</span></div></a> */}
                 </div>
             </Fade>
 
